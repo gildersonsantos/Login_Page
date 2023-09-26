63dac766f05721d4012dd4c36fc72fedaf39ce5f
Gilderson Santos <122750614+gildersonsantos@users.noreply.github.com>
2023-09-26 07:44:30 -0300

    Criação de alerta personalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-inputPassword = document.getElementById("fpassword")
-btnMostrarSenha = document.getElementById("btn-mostrar-senha")
+document.addEventListener("click", (event) => {
+    const el = event.target;
 
-btnMostrarSenha.addEventListener("click", () => {
-    if (inputPassword.type === "password") {
-        inputPassword.setAttribute("type", "text")
-        btnMostrarSenha.classList.replace("bi-eye-fill", "bi-eye-slash-fill")
-    } else if (inputPassword.type === "text") {
-        inputPassword.setAttribute("type", "password")
-        btnMostrarSenha.classList.replace("bi-eye-slash-fill", "bi-eye-fill")
+    if (el.tagName.toLowerCase() === "i" && el.id.toLowerCase() === "btn-mostrar-senha") {
+        inputPassword = document.getElementById("fpassword")
+        btnMostrarSenha = document.getElementById("btn-mostrar-senha")
+
+        if (inputPassword.type === "password") {
+            inputPassword.setAttribute("type", "text")
+            btnMostrarSenha.classList.replace("bi-eye-fill", "bi-eye-slash-fill")
+        } else if (inputPassword.type === "text") {
+            inputPassword.setAttribute("type", "password")
+            btnMostrarSenha.classList.replace("bi-eye-slash-fill", "bi-eye-fill")
+        }
+    } 
+    else if (el.tagName.toLowerCase() === "button" && el.parentElement.id.toLowerCase() === "alerta") {
+        alertaCaixa = document.getElementById("alerta");
+        alertaCaixa.classList.add("fechar");
+        document.body.classList.remove("deactive");
     }
 })
+
+// verificarPagina = setInterval(() => {
+//     alertaCaixa = document.getElementById("alerta");
+
+//     if (!((!document.body.classList.contains("deactivate")) && alertaCaixa.classList.contains("fechar"))) {
+//         window.location.reload(false);
+//     }
+// }, 100);
